18339ba62ddc76885580cbd18a7ff87161321629
wogus <wogus@DESKTOP-KNV88FV>
2021-03-09 17:21:58 +0900

    구구단

---
+++ src/Gugudan.java
@@ -0,0 +1,30 @@
+
+public class Gugudan {
+	public static void main(String[] args) {
+		//2단
+		System.out.println("2단");
+		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		System.out.println(2 * 5);
+		System.out.println(2 * 6);
+		System.out.println(2 * 7);
+		System.out.println(2 * 8);
+		System.out.println(2 * 9);
+		System.out.println("---------------");
+		//3단
+		System.out.println("3단");
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);	
+		System.out.println(3 * 5);	
+		System.out.println(3 * 6);	
+		System.out.println(3 * 7);	
+		System.out.println(3 * 8);	
+		System.out.println(3 * 9);	
+//이클립스 단축키 사용 및 한줄 복사 안될 때 해결 방법
+	
+	}
+}
