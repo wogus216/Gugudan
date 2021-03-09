44ddeef43a5b549df9e98d1db00c27290b8b2a38
wogus <wogus@DESKTOP-KNV88FV>
2021-03-09 18:06:31 +0900

    변수의 활용, 머리 한대 맞은 기분이다.

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,30 +1,66 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		//2단
-		System.out.println("2단");
-		System.out.println(2 * 1);
-		System.out.println(2 * 2);
-		System.out.println(2 * 3);
-		System.out.println(2 * 4);
-		System.out.println(2 * 5);
-		System.out.println(2 * 6);
-		System.out.println(2 * 7);
-		System.out.println(2 * 8);
-		System.out.println(2 * 9);
-		System.out.println("---------------");
-		//3단
-		System.out.println("3단");
-		System.out.println(3 * 1);
-		System.out.println(3 * 2);
-		System.out.println(3 * 3);
-		System.out.println(3 * 4);	
-		System.out.println(3 * 5);	
-		System.out.println(3 * 6);	
-		System.out.println(3 * 7);	
-		System.out.println(3 * 8);	
-		System.out.println(3 * 9);	
-//이클립스 단축키 사용 및 한줄 복사 안될 때 해결 방법
+//		//2단
+//		System.out.println("2단");
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+//		System.out.println(2 * 6);
+//		System.out.println(2 * 7);
+//		System.out.println(2 * 8);
+//		System.out.println(2 * 9);
+//		System.out.println("---------------");
+//		//3단
+//		System.out.println("3단");
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);	
+//		System.out.println(3 * 5);	
+//		System.out.println(3 * 6);	
+//		System.out.println(3 * 7);	
+//		System.out.println(3 * 8);	
+//		System.out.println(3 * 9);	
+		
+		//4단
+		
+	System.out.println("4단 출력을 위해 4를 입력해주세요");
+	Scanner sc = new Scanner(System.in);
+	int number = sc.nextInt();
+	int result = number * 1;
+	System.out.println(result);
+	result = number * 2;
+	System.out.println(result);
+	result = number * 3;
+	System.out.println(result);
+	result = number * 4;
+	System.out.println(result);
+	result = number * 5;
+	System.out.println(result);
+	result = number * 6;
+	System.out.println(result);
+	result = number * 7;
+	System.out.println(result);
+	result = number * 8;
+	System.out.println(result);
+	result = number * 9;
+	System.out.println(result);
+	
+	System.out.println("구구단 출력을 위해 숫자를 입력해주세요");
+	int number1 = sc.nextInt();
+	System.out.println(number1 * 1);
+	System.out.println(number1 * 2);
+	System.out.println(number1 * 3);
+	System.out.println(number1 * 4);
+	System.out.println(number1 * 5);
+	System.out.println(number1 * 6);
+	System.out.println(number1 * 7);
+	System.out.println(number1 * 8);
+	System.out.println(number1 * 9);
 	
 	}
 }
