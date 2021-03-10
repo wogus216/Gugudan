201490576473dd56f33858c05e5e99422351f74a
wogus <wogus@DESKTOP-KNV88FV>
2021-03-10 22:43:37 +0900

    메소드 추가,쉽지않다. 더 열심히 노력해야지

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,33 +1,24 @@
-import java.util.Scanner;
+import javax.xml.transform.Result;
 
 public class Gugudan {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] result = new int[9];
-		for(int j=2; j<10; j++) {
-			for(int i=0; i<result.length; i++){
-				result[i] = j*(i+1);
-			}
-			for(int i=0; i<result.length; i++) {
-				System.out.println(result[i]);
-			}
+	public static int[] calculate(int times) {
+		int[] result= new int[9];
+		for(int i=0; i<result.length; i++) {
+			result[i]= times*(i+1);
+		}
+			return result;
+
+	}	
+	public static void print(int[] result) {
+		for(int i=0; i<result.length; i++) {
+			System.out.println(result[i]);
 		}
 		
-		
-//		내 버전!!		
-//		System.out.println("값을 입력해주세요");
-//		Scanner sc = new Scanner(System.in);
-//		int[] result = new int[9];
-//	
-//		for(int i=2; i<10; i++) {
-//			if(i ==2) {
-//				System.out.println("2단은 안됩니다.");
-//			} else if(i==9) {
-//				System.out.println("9단은 안됩니다.");
-//			}else {
-//				for(int j=0; j<result.length; j++)
-//					System.out.println(i*j);
-//			}
-//		}
 	}
+	public static void main(String[] args) {
+		for(int i=2; i<10; i++) {
+		int[] result= calculate(i);
+		print(result);
+			}
+		}
 }
