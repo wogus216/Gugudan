c8189247faaa11e63c96308ba081378190eea918
wogus <wogus@DESKTOP-KNV88FV>
2021-03-11 11:14:25 +0900

    저장하자

---
+++ src/Gugudan.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Gugudan {
+public static int[] calculate(int times) {
+	int[] result= new int[times];
+	for(int i=0; i<result.length; i++) {
+		result[i]= times*(i+1);
+	}
+	return result;
+}
+	public static void print(int[] result) {
+		for(int i=0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	}
+
